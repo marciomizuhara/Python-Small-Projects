bb349b7b1fae77b5c09991d7e51a1f1a92a7dee3
marciomizuhara <marciomizu@gmail.com>
2022-11-05 11:22:28 -0300

    created website snapshot project

---
+++ Website-Snapshot/main.py
@@ -0,0 +1,29 @@
+import sys
+import selenium
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import chromedriver_binary
+
+script_name = sys.argv[0]
+options = Options()
+options.add_argument('--headless')
+driver = webdriver.Chrome(options=options)
+
+
+try:
+    url = sys.argv[1]
+    driver.get(url)
+    page_width = driver.execute_script('return document.body.scrollWidth')
+    page_height = driver.execute_script('return document.body.scrollHeight')
+    driver.set_window_size(page_width, page_height)
+    driver.save_screenshot('screenshot.png')
+    driver.quit()
+    print('Snapshot done successfully')
+except IndexError:
+    print('Usage: %s URL' % script_name)
+
+
+
+
+
+
